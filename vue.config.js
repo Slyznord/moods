def8c7bdaae2010ee3f0b24f8d8c67727db3c515
Maikl Tadesse <mit@superdoc.ru>
2024-01-05 16:40:05 +0200

    added vue pwa

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV ? '/moods/' : '/'
+  publicPath: process.env.NODE_ENV ? '/moods/' : '/',
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js'
+    }
+  }
 })
